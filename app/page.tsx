9796c5ea131083ade604d85b74167fd60e694d66
0xChicharito <133844082+0xChicharito@users.noreply.github.com>
2024-08-10 18:13:38 +0700

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import { FaTwitter, FaTelegramPlane } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome to &nbsp;
           <code className="font-mono font-bold">Node 9X</code>
         </p>
@@ -25,22 +24,6 @@ export default function Home() {
               priority
             />
           </a>
-          <div className="flex items-center gap-4 ml-4">
-            <a
-              href="https://twitter.com/0xchicharito"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitter size={24} className="text-blue-500 dark:text-white" />
-            </a>
-            <a
-              href="https://t.me/Oxchicharito"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTelegramPlane size={24} className="text-blue-400 dark:text-white" />
-            </a>
-          </div>
         </div>
       </div>
 
