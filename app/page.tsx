d6f7b3caaa611e389dfbc01cab85f48325b1a74f
0xChicharito <133844082+0xChicharito@users.noreply.github.com>
2024-08-10 18:19:08 +0700

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://service.node9x.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -60,7 +60,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://service/node9x.com"
+          href="https://explorer.node9x.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -77,7 +77,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://explore.node9x.com"
+          href="https://explorer.node9x.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
